0ceca85618d8326d34942a4dc0daf29dbb79e749
elpss <122477039+elpss@users.noreply.github.com>
2026-05-09 11:35:11 -0300

    page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'Arial'
+    }}>
+      <h1>Sorteio Instagram Online 🚀</h1>
+    </div>
+  )
+}
